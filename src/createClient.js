ddbf2d7f826236c9bbdb4673c683265f96ccd5c3
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-10-07 02:46:16 +0530

    fix .gitignore and added supabase api keys

--- src/createClient.js
+++ src/createClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const apiUrl = 'https://vuddufpsepkxatpvubqq.supabase.co';
-const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1ZGR1ZnBzZXBreGF0cHZ1YnFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3NzkxODAsImV4cCI6MjA3NTM1NTE4MH0.TZtm75GTzhB6D6g66Eexf4qBC3Wdi0CpVAJzgXsiNiQ';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(apiUrl,apiKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
