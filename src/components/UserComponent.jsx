1b6446d52a8bcad5cac53434e136c112ec54ee4f
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-10-08 01:41:30 +0530

    feat: Add user creation form to UserComponent
    - Introduce new state for individual user data (name and age)
    - Add createUser async function to insert user into Supabase 'users' table
    - Implement handleChange for form inputs and render form with name, age fields, and submit button
    - This enables creating new users directly from the component, enhancing user management functionality

--- src/components/UserComponent.jsx
+++ src/components/UserComponent.jsx
@@ -5,9 +5,11 @@ import { useState } from 'react'
 
 const UserComponent = () => {
 
-    const [users,setUsers] = useState([]);
-
-    console.log(users);
+  const [users,setUsers] = useState([]);
+  const [user,setUser] = useState({
+    name : '',
+    age : ''
+  });
 
 
     useEffect (()=>{
@@ -21,11 +23,49 @@ const UserComponent = () => {
     setUsers(data)
     console.log(data);
   }
+
+  async function createUser() {
+    await supabase
+    .from('users')
+    .insert({name: user.name, age : user.age})
+  }
+
+  const handleChange = (e) => {
+    
+    setUser(prevFromData => {
+      return{
+        ...prevFromData,
+        [e.target.name]:e.target.value
+      }
+    })
+  }
+
   return (
     <div>
+
+
+    <form onSubmit={createUser}>
+      <input
+       type="text"
+       placeholder='enter name'
+       name='name'
+       onChange={(handleChange)}
+       />
+       
+       <input
+       type="number"
+       placeholder='enter age'
+       name='age'
+       onChange={(handleChange)}
+       />
+      <button type='submit'>Create</button>
+    </form>
+
+
+
         <table>
           <thead>
-            <tr>
+            <tr key={user.id}>
               <th>Id</th>
               <th>Name</th>
               <th>Age</th>
